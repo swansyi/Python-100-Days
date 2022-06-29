85d9dc38fae497d98cc72cf9b850f23f8cb5da16
swansyi <swansyi@outlook.com>
2022-06-29 19:38:22 +0800

    6/29 study

--- Day01-15/code/Day07/set2.py
+++ Day01-15/code/Day07/set2.py
@@ -18,6 +18,7 @@ def main():
     set2 = set(range(2, 11, 2))
     print(set2)
     set3 = set(range(1, 5))
+    print(set3)
     print(set1 & set2)
     # print(set1.intersection(set2))
     print(set1 | set2)
